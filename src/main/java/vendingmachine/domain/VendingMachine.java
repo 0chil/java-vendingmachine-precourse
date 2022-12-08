d3a663acbb7d291b3ea72dd1b9a5ee2aad8462d0
0chil <0@chll.it>
2022-12-08 17:28:29 +0900

    refactor: 패키지 정리

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -4,8 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import vendingmachine.Product;
-
 public class VendingMachine {
     private final int embeddedAmount;
     private final Map<String, Product> products;
